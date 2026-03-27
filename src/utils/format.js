c38ed733cbc07fd873391af09543cb7e6dc21b30
iivyyww <amafuww66@gmail.com>
2026-03-27 11:48:54 -0400

    Update format.js

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,1 +1,9 @@
+export const shortenAddress = (address) => {
+  if (!address) return "";
+  return `${address.slice(0, 6)}...${address.slice(-4)}`;
+};
 
+export const safeDisplay = (value) => {
+  if (value === null || value === undefined || value === "") return "-";
+  return value;
+};
