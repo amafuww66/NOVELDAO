9e77d46d6807343b36fa3532c55258f0386ccce7
iivyyww <amafuww66@gmail.com>
2026-03-27 11:48:26 -0400

    Update TreasuryPanel.js

--- src/components/TreasuryPanel.js
+++ src/components/TreasuryPanel.js
@@ -1,1 +1,15 @@
+import React from "react";
 
+function TreasuryPanel() {
+  return (
+    <div className="card">
+      <h2 className="section-title">Treasury Panel</h2>
+      <p>Treasury ETH Balance: To be loaded from contract</p>
+      <p>Treasury Token Balance: To be loaded from contract</p>
+      <p>Pending Rewards: To be loaded from contract</p>
+      <p>Executed Distributions: To be loaded from contract</p>
+    </div>
+  );
+}
+
+export default TreasuryPanel;
