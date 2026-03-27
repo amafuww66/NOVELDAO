8f858784ab5005422cb77d7ff20cda2fb59d963a
iivyyww <amafuww66@gmail.com>
2026-03-27 11:54:17 -0400

    Update Overview.js

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,1 +1,18 @@
+import React from "react";
 
+function Overview() {
+  return (
+    <div className="card">
+      <h2>Overview</h2>
+
+      <p>Total Supply: -</p>
+      <p>Treasury Balance: -</p>
+      <p>Quorum: -</p>
+      <p>Voting Period: -</p>
+      <p>Current Round: -</p>
+      <p>Status: -</p>
+    </div>
+  );
+}
+
+export default Overview;
